c2ff8922cd2d639cfb56efb00efbc2055578264b
Vuln Lol <vulnlol@pm.me>
2024-09-30 13:25:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# Data Search Engine
+ 
+Have the ability to search billions of datasets that are **manually imported** retroactively!
+
+Fork of Search.0t.rocks
+
+## Instructions
+1. Clone the repo `git clone "https://github.com/vulnlol/DataSearchEngine"`
+2. Enter into the repo `cd DataSearchEngine`
+3. Run `sh ./build-and-run.sh` or `docker compose build; docker compose up`
+4. After that is complete do `sh ./add-solar-records.sh`
+
+## Adding Data (DO THIS AFTER FOLLOWING THE INSTRUCTIONS ABOVE)
+1. Download Solr from [here](https://solr.apache.org/downloads.html) using the **BINARY** release
+2. Extract and enter Solr
+3. Then enter `sh bin/post -c BigData -p 8983 -host 127.0.0.1:8983 FILE` where you replace file with the actual path for the file containing the data.
+
+## Improvements
+- Enhanced styling across all pages and those lacking any at all
+- Implemented a json based login system
+- Admin Panel (not implemented yet)
+- Search News For Keywords (not implemented yet)
+
+## Admin Panel
+**( This Isn't Implemented Yet )**
+I will be introducing a admin panel in the future, it will have the ability to toggle user logins, the ability to toggle user registration, force password resets, and a way to add upload datasets to the databases.
+Though the default login is `admin:password`
+
+## Contributions
+I'm open to any and all contribtuions and suggestions to the search engine. Feel free to make a pull request and if all goes well then it will be merged.
+
+## Demo
+**REMOVED**
+
+<details><summary>Search Field Parameters</summary><br>
+* Accuracy Radius
+* Address
+* ASN
+* ASN Org
+* Auto Body
+* Auto Class
+* Auto Make
+* Auto Model
+* Auto Year
+* Birth Year
+* Birth Month
+* Birthday
+* City
+* Continent
+* Country
+* DOB
+* Domain
+* Emails
+* Ethnicity
+* FirstName
+* MiddleName
+* Gender
+* Income
+* IPs
+* LastName
+* LatLong
+* Line
+* Links
+* Location
+* Notes
+* Party
+* Passwords
+* Phone Numbers
+* Photos
+* Source
+* State
+* Usernames
+* VIN
+* Zip Code
+* VRN
+* SSN
+* License Number
+* Debit Number
+* Debit Expiration
+* Debit Pin
+* Credit Number
+* Credit Expiration
+* Credit Pin
+* Passport Number
+* Military ID
+* Bank Account Numbers
+* Schools Attended
+* Certifications
+</details>
+
+
+<details>
+<summary>Why is this open sourced?</summary>
+<br>
+ 
+Originally, I had not planned to open source this project or publish it online at all since I was going to run it on a local system. However, a turn of events has led me to reconsider this decision. A former collaborator, who was part of the project but did not contribute to the backend development, coding, or design—areas where I exclusively worked—began to show an unexpected interest in the detailed setup and operation of the project just before their departure. They did not contribute any suggestions or code during their time with the project.
+
+Their inquiries intensified suddenly, focusing on how to get the project started, where the problems in the original code were, and how various components were supposed to function. This occurred despite the known issues with the original version hosted at searchl.0t.rocks, which included a non-functional wallet system and broken setup scripts.
+
+After they left, I learned through mutual contacts at a convention that they were attempting to sell the project despite it having an Apache V2 license. Given these circumstances, I have decided to open source the project to protect its integrity and ensure that its use aligns with my terms and conditions. This decision is meant to reflect the true scope of my contributions and safeguard the project's original vision.
+
+Was going to call it valor intelligence search engine if you are wondering what the names are in the various pages.
+
+Ps, fuck you
+</details>
+
+
---
+++ add-solor-records.sh
@@ -0,0 +1,67 @@
+echo "Creating Database"
+echo "If you have defined a different port for your solr server please edit this file's linked ports"
+
+# Create collection for data
+curl "http://127.0.0.1:8983/solr/admin/collections?action=CREATE&name=BigData&numShards=8&replicationFactor=3&wt=json" 
+
+
+# Disable schemaless mode
+
+echo "Disabling schema-less mode"
+curl "http://127.0.0.1:8983/solr/BigData/config" -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
+
+# Add Schema
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"accuracy_radius","type":"pint"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"address","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"asn","type":"pint","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"asnOrg","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"autoBody","type":"string"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"autoClass","type":"string"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"autoMake","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"autoModel","type":"string"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"autoYear","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"birthYear","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"birthMonth","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"birthday","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"city","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"continent","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"country","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"dob","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"domain","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"emails","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"ethnicity","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"firstName","type":"text_general","uninvertible":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"middleName","type":"text_general","uninvertible":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"gender","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"false","name":"income","type":"string"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"true","name":"ips","type":"string","multiValued":"true","omitNorms":"true","omitTermFreqAndPositions":"true","sortMissingLast":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"lastName","type":"text_general","uninvertible":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"true","name":"latLong","type":"location","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"false","uninvertible":"false","name":"line","type":"string"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"false","uninvertible":"false","name":"links","type":"string","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"true","name":"location","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"false","uninvertible":"false","name":"notes","type":"string","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"false","uninvertible":"false","name":"party","type":"string","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"passwords","type":"string","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"phoneNumbers","type":"text_general"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"false","uninvertible":"false","name":"photos","type":"string","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"source","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","uninvertible":"false","name":"state","type":"string","docValues":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"usernames","type":"text_general","uninvertible":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"vin","type":"text_general","uninvertible":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"zipCode","type":"string","uninvertible":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"stored":"true","indexed":"true","name":"VRN","type":"text_general","uninvertible":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"ssn","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"licenseNumber","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"debitNumber","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"debitExpiration","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"debitPin","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"creditNumber","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"creditExpiration","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"creditPin","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"passportNumber","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"militaryID","type":"string","stored":"true","indexed":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"bankAccountNumbers","type":"string","stored":"true","indexed":"true","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"schoolsAttended","type":"string","stored":"true","indexed":"true","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"certifications","type":"string","stored":"true","indexed":"true","multiValued":"true"}}'
+curl 'http://127.0.0.1:8983/solr/BigData/schema?wt=json' -X POST -H 'Accept: application/json' --data-raw '{"add-field":{"name":"politicalAffiliation","type":"string","stored":"true","indexed":"true"}}'
